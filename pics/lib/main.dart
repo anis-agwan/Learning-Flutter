639975b99ab9a1f7655737752eec19613ab37d43
Mohmed Anis Agwan <anishagwan@gmail.com>
2021-11-07 21:52:58 +0530

    Added a appbar and a floating action button

--- pics/lib/main.dart
+++ pics/lib/main.dart
@@ -4,9 +4,15 @@ import 'package:flutter/material.dart';
 // Main func to run when app starts
 void main() {
   // Create a new Text Widget to show on screen
-  // ignore: unused_local_variable
+  // ignore: unused_local_variable, prefer_const_constructors
   var app = MaterialApp(
-    home: Text("Hello, Friend"),
+    // ignore: prefer_const_constructors
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text("Let's see some images"),
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: () {  },),
+    ),
   );
 
   // Take the widget and get it on the screen
