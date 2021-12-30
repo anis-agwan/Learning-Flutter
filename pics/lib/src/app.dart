75ac35454e83d0ff344ce9a098157749ef07f5c4
Mohmed Anis Agwan <anishagwan@gmail.com>
2021-12-30 19:52:42 +0530

    Refactored stateless class into  stateful

--- pics/lib/src/app.dart
+++ pics/lib/src/app.dart
@@ -2,9 +2,16 @@
 import 'package:flutter/material.dart';
 
 // Create a custom widget class
+class App extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return AppState();
+  }
+}
 // extends the Stateless Widget base class
-class App extends StatelessWidget {
+class AppState extends State<App> {
   // Must define a build method that returns the widgets that this widget will show
+  int counter = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,8 +20,13 @@ class App extends StatelessWidget {
         appBar: AppBar(
           title: const Text("Let's see some images"),
         ),
+        body: Text("$counter"),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {  },
+          onPressed: () {
+            setState(() {
+              counter += 1;
+            });
+          },
           // ignore: prefer_const_constructors
           child: Icon(
               Icons.add
