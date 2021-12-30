0f97e22e724b516c8bb561fe8279bcf73f0191f3
Mohmed Anis Agwan <anishagwan@gmail.com>
2021-12-30 19:15:52 +0530

    Created a custom class of Stateless widget

--- pics/lib/main.dart
+++ pics/lib/main.dart
@@ -1,26 +1,12 @@
 // Importing helper library from flutter
 import 'package:flutter/material.dart';
+import 'src/app.dart';
 
 // Main func to run when app starts
 void main() {
   // Create a new Text Widget to show on screen
   // ignore: unused_local_variable, prefer_const_constructors
-  var app = MaterialApp(
-    // ignore: prefer_const_constructors
-    home: Scaffold(
-      appBar: AppBar(
-        title: const Text("Let's see some images"),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {  },
-        // ignore: prefer_const_constructors
-        child: Icon(
-          Icons.add
-        ),
-      ),
-    ),
-  );
 
   // Take the widget and get it on the screen
-  runApp(app);
+  runApp(App());
 }
--- pics/lib/src/app.dart
+++ pics/lib/src/app.dart
@@ -3,5 +3,25 @@ import 'package:flutter/material.dart';
 
 // Create a custom widget class
 // extends the Stateless Widget base class
+class App extends StatelessWidget {
+  // Must define a build method that returns the widgets that this widget will show
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // ignore: prefer_const_constructors
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Let's see some images"),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {  },
+          // ignore: prefer_const_constructors
+          child: Icon(
+              Icons.add
+          ),
+        ),
+      ),
+    );
+  }
 
-// Must define a build method that returns the widgets that this widget will show
+}
